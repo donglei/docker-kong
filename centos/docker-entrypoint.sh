63dac23a88002bbaef2f1fa66dde810319a64a6e
donglei <donglei@debiandong>
2017-12-13 19:33:57 +0800

    add prometheus

--- centos/docker-entrypoint.sh
+++ centos/docker-entrypoint.sh
@@ -7,6 +7,9 @@ export KONG_NGINX_DAEMON="off"
 # Setting default prefix (override any existing variable)
 export KONG_PREFIX="/usr/local/kong"
 
+export KONG_LUA_PACKAGE_PATH="$KONG_CUSTOM_PLUGINS_PATH/?.lua;;"
+export KONG_CUSTOM_PLUGINS="prometheus"
+
 # Prepare Kong prefix
 if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
 	kong prepare -p "/usr/local/kong"
